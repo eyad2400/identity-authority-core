48aa5fa900f133ba9c3f0cd069dd8513595715df
eyad2400 <52995692+eyad2400@users.noreply.github.com>
2025-12-29 10:45:33 +0200

    Update iac.config.js

---
+++ backend/config/iac.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  STAFF_TYPES: ["officer", "personnel", "civil"],
+  ENGAGEMENT_TYPES: ["candidate", "trainee"],
+  STAFF_STATUS: ["active", "ended"],
+  TRAINEE_SOURCE: "ga"
+};
